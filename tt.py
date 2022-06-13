5263d85e1fa27e93fc4b8129d6751e17519b52b0
ur mom <46662314+UCYT5040@users.noreply.github.com>
2022-06-12 23:24:07 -0500

    fix removed code

--- tt.py
+++ tt.py
@@ -45,3 +45,38 @@ time.sleep(1)
 print("Wallet found")
 
 time.sleep(0.2)
+
+def file_len():
+    with open("proxies.txt") as f:
+        for i, _ in enumerate(f):
+            pass
+    return i
+print(f"""
+[1] Use Proxies
+[2] Don't use Proxies
+""")
+proxies = input("Do oyu want to use Proxies?: ")
+if proxies == "1":
+    print(Fore.GREEN + "Imported " + str(file_len()) + " Proxies")
+else:
+    print("Not using Proxies")
+time.sleep(0.2)
+print(Fore.BLUE+ "Setting up workspace for you...")
+time.sleep(3)
+
+def id_gen(size=40, chars=string.ascii_uppercase + string.digits):
+    return "".join(random.choice(chars) for _ in range(size))
+
+tries = 0
+
+while(True):
+    if(tries > random.randint(10000, 100000)): # chance to get fake btc
+        print(Fore.CYAN +"[-]"+ Fore.RED + " bc1" + id_gen() + Fore.GREEN +" |  Valid  |  " + str(round(random.uniform(0,2), 4)), "BTC")
+        print(Fore.GREEN +"Withdrawing to your Wallet...")
+        time.sleep(10)
+        tries = 0
+        print(Fore.GREEN + "Done!")
+        time.sleep(1)
+    else:
+        print(Fore.CYAN +"[-]"+ Fore.RED + " bc1" + id_gen() + Fore.CYAN +" | InValid |  " + "0.0000 BTC")
+        tries += 1
